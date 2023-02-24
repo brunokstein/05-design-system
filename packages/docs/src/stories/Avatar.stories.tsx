1c56931e17b6b0f1fbf5d24542bd3f8427df3348
Bruno Kempfer Stein <brunokempferstein@MacBook-Pro-de-Bruno.local>
2023-02-24 14:52:15 -0300

    ok

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/brunokstein.png',
     alt: 'Bruno Stein',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
